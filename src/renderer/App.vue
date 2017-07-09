1141049337c4670220e45c64706cc0a47d000ce7
Alexander <alexander@Alexanders-Mac-mini.local>
2017-07-09 16:11:46 +0200

    push

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,43 +1,10 @@
 <template>
-  <div style="webkit-app-region: drag">
-    <carousel>
-      <carouselSlide v-for="n in slides">
-        <div class="text">Salut les gens</div>
-        <img :src="'http://lorempicsum.com/futurama/800/450/' + n" width='100%'>
-      </carouselSlide>
-    </carousel>
-  </div>
+
 </template>
 
 <script>
-  import carousel from './components/carousel.vue'
-  import carouselSlide from './components/carouselSlide.vue'
-
-  export default {
-    data () {
-      return {
-        slides: 6
-      }
-    },
-    components: { carousel, carouselSlide }
-  }
 
 </script>
 
 <style lang="sass">
-body
-  padding: 0
-  margin: 0
-img
-  user-drag: none;
-  -webkit-user-drag: none;
-.text
-  position: absolute
-  color: white
-  left: 0
-  right: 0
-  text-align: center
-  top: 50%
-  font-size: 2em
-  font-family: arial
 </style>
