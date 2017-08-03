edcf8dba1033791864afb4ed8fc0d8286b64c4c2
blaadje <acharlot91@gmail.com>
2017-08-03 13:41:57 +0200

    Adding id ordering

--- src/renderer/components/MyComponent.vue
+++ src/renderer/components/MyComponent.vue
@@ -2,6 +2,7 @@
   .userList
     transition-group(name="fade", tag="ul")
       li(v-for="(user, index) in users", key="index") {{ user.name }}
+        p {{ user['.key'] }}
         button(@click.prevent="deleteUser(user)") X
     input(type="text", ref="name", @keydown.enter="addUser")
 </template>
@@ -10,35 +11,39 @@
 import Firebase from 'firebase'
 
 var config = {
-  apiKey: 'AIzaSyBz73_KOl_AYMfeJnBQ7lBCyH-KuSUQsqQ',
-  authDomain: 'projet-tets.firebaseapp.com',
   databaseURL: 'https://projet-tets.firebaseio.com',
-  projectId: 'projet-tets',
-  storageBucket: 'projet-tets.appspot.com',
-  messagingSenderId: '304277615671'
+  projectId: 'projet-tets'
 }
 var firebaseApp = Firebase.initializeApp(config)
 var db = firebaseApp.database()
+var ref = db.ref('users')
 
 export default {
   firebase () {
     return {
-      users: db.ref('users')
-    }
-  },
-  created () {
-    return {
-      users: db.ref('users')
+      users: ref.orderByKey()
     }
   },
   methods: {
     addUser () {
       this.newUser = this.$refs.name.value
-      db.ref('users').push({ name: this.$refs.name.value })
+      // db.ref('users').push({ name: this.$refs.name.value })
+      ref.child(this.getkey()).set({ name: this.$refs.name.value })
       this.$refs.name.value = ''
     },
     deleteUser (name) {
-      db.ref('users').child(name['.key']).remove()
+      ref.child(name['.key']).remove()
+    },
+    getkey () {
+      // let key = Math.random().toString(36).substring(3)
+      let date = new Date()
+      let yy = date.getFullYear()
+      let mm = date.getMonth()
+      let dd = date.getDay()
+      let m = date.getMinutes()
+      let s = date.getSeconds()
+      let result = '-' + yy + '' + mm + '' + dd + '' + m + '' + s
+      return result
     }
   }
 
@@ -51,11 +56,12 @@ export default {
     margin: 0
     padding: 0
     li
-      transition: transform 1s ease
       list-style-type: none
       padding: 1em
       &:not(:last-child)
         border-bottom: 1px solid grey
+      &.fade-active
+        transition: transform 1s ease
       &.fade-enter
         transform: translateX(-400px)
       &.fade-enter-to
